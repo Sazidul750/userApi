9be9acbfeb126001b37c7a456487d42007776b74
sazidul750 <sazidulok69@gmail.com>
2023-07-28 12:36:12 -0700

    app and file create done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+const url='https://jsonplaceholder.typicode.com/photos'
 export default function App() {
-  return (
-    <div>
-      <h1>sazidul islam</h1>
-    </div>
-  )
+  const [photos,setPhotos]=useState(null)
+  const [load,setLoad]=useState(true)
+  const [error,setError]=useState(null)
+
+  const useData= async(url)=>{
+    try {
+      const res= await fetch(url);
+      const data=await res.json();
+      setPhotos(data)
+      setLoad(false)
+      setError(null)
+      console.log(photos)
+    } catch (error) {
+      setError(error)
+      setLoad(false)
+    }
+  
+  }
+  useEffect(()=>{
+    useData(url)
+  },[url])
+
+  return {photos,error,load}
 }
